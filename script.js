e81911adad3dc504973c749b2bc60182b7442b5a
Adarsh11234 <aadarshpatil123@gmail.com>
2025-01-29 11:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+ function validateAndSubmit(event) {
+            event.preventDefault();
+            
+            let name = document.getElementById("name").value.trim();
+            let age = document.getElementById("age").value.trim();
+            
+            if (!name || !age) {
+                alert("Both fields are required.");
+                return;
+            }
+            
+            age = Number(age);
+            
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    age > 18 ? resolve() : reject();
+                }, 4000);
+            })
+            .then(() => alert(`Welcome, ${name}. You can vote.`))
+            .catch(() => alert(`Oh sorry ${name}. You aren't old enough.`));
+        }
